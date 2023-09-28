71c205262896e8306eb7bc6263294ae58f468b63
Virginia Zurita Ortiz <virginiazuor@gmail.com>
2023-09-28 15:12:24 +0200

    some changes typo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 
 
 import { UseQueryResponse, UseQueryState, gql, useQuery } from "urql";
-import { Card, SimpleGrid, Heading, Box } from '@chakra-ui/react'
+import { Card, SimpleGrid, Heading, Box, Text } from '@chakra-ui/react'
 import { Fragment } from "react";
 
 const query = gql`
@@ -25,11 +25,11 @@ const HomePage = () => {
   
 
   return <Box width={'100vw'} padding={10} display={"flex"} flexDirection={"column"} gap={10} alignItems={'center'} justifyContent={"center"}> 
-    <Heading>Home</Heading>
+    <Heading >Welcome to Star Wars Database</Heading>
     
     {data.data ? <SimpleGrid columns={[2, null, 3]} spacing={10} width={'100%'}>{data.data.allPeople.edges.map((char:any)=>
-    (<Card key={char.node.id} height={50} display={"flex"} textAlign={"center"} justifyContent={"center"} alignItems={"center"} onClick={()=>console.log(char.node.name)}>
-      <h3>{char.node.name}</h3>
+    (<Card key={char.node.id} height={'100px'} cursor={'pointer'} display={"flex"} textAlign={"center"} justifyContent={"center"} alignItems={"center"} onClick={()=>console.log(char.node.name)}>
+      <Text fontSize='xl' textTransform={'uppercase'}>{char.node.name}</Text>
 
     </Card>))}</SimpleGrid>: 
     <h2>Loading...</h2> }
