d99f7969f77b263cee56d64e9cddaa04521ea210
Virginia Zurita Ortiz <virginiazuor@gmail.com>
2023-09-28 23:11:16 +0200

    character detail page fetch data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { UseQueryResponse, gql, useQuery } from "urql";
 import { Card, SimpleGrid, Heading, Box, Text } from "@chakra-ui/react";
-import { Link, Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const query = gql`
   query Home {
@@ -33,7 +33,7 @@ const HomePage = () => {
       {data.data ? (
         <SimpleGrid columns={[2, null, 3]} spacing={10} width={"100%"}>
           {data.data.allPeople.edges.map((char: any) => (
-            <Link key={char.node.id} to={`/person/:${char.node.id}`}>
+            <Link key={char.node.id} to={`/person/${char.node.id}`}>
               <Card
                 height={"100px"}
                 cursor={"pointer"}
--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,5 +1,85 @@
+import { Box, Card } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {  gql, stringifyVariables, useQuery } from "urql";
+
 const PersonPage = () => {
-  return <div>Person</div>;
+  /* const client = createClient({
+    url: 'https://swapi-graphql.netlify.app/.netlify/functions/index', 
+  }); */
+
+
+  const id = useParams();
+  const query = gql`
+    query currentPerson($personId: ID) {
+      person(id: $personId) {
+        id
+        name
+        birthYear
+        eyeColor
+        filmConnection {
+          edges {
+            node {
+              producers
+              id
+              releaseDate
+              title
+              planetConnection {
+                edges {
+                  node {
+                    id
+                    name
+                    surfaceWater
+                  }
+                }
+              }
+            }
+          }
+        }
+        species {
+          averageHeight
+          name
+          id
+        }
+      
+      }
+      
+    }
+   
+  `;
+  const variables = {
+    personId: stringifyVariables(id),
+  };
+
+ /* good?
+   client.query(query, variables).toPromise().then((result) => {
+  console.log(result.data); // Aquí puedes acceder a los datos de la respuesta
+});  */
+
+  const data = useQuery( {query, variables} );
+  const [personResult, setPersonResult] = useState<any>()
+
+  useEffect(() => {
+    setPersonResult(data[0].data?.person)
+  
+   
+  }, [data[0].data?.person])
+  
+
+
+  console.log({personResult});
+  
+
+  return <Box>{
+    personResult ? <Card>
+<h1>{personResult.name}</h1>
+<h4>Producers: </h4>
+<h4>Species: {personResult.species?.name || "Human"}</h4>
+{personResult.species && <h4>Average Height: {personResult.species.averageHeight}</h4>}
+<h4>Films:</h4>
+{/* films...etc */}
+    </Card>: <h1>Loading...</h1>
+    }</Box>;
 };
 
 export default PersonPage;
